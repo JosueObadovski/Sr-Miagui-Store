837ef22d1c9782572cbbcf905e924f55c95e046b
JosueObadovski <josueantoniogardasz@hotmail.com>
2023-06-03 00:35:22 -0300

    Edição inicial na estilização da tela de cadastro

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,15 @@
 function showScreen1() {
-  document.getElementById("screen1").style.display = "block"
-  document.getElementById("screen2").style.display = "none"
-  document.getElementById("showScreen2").style.display = "block" // Exibe o botão da tela 2
+  document.getElementById("screen1").style.display = "block";
+  document.getElementById("screen2").style.display = "none";
+  document.getElementById("showScreen2").style.display = "block"; // Exibe o botão da tela 2
 }
 
 function showScreen2() {
-  document.getElementById("screen1").style.display = "none"
-  document.getElementById("screen2").style.display = "block"
+  document.getElementById("screen1").style.display = "none";
+  document.getElementById("screen2").style.display = "block";
 }
 
 // Evento de clique para carregar a tela 1 e exibir o botão da tela 2
-document.getElementById("showScreen1").addEventListener("click", function () {
-  showScreen1()
-})
+document.getElementById("showScreen1").addEventListener("click", function() {
+  showScreen1();
+});
