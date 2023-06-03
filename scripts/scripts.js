527ef553c6896b917d0089813e2afd69cb6aaece
JosueObadovski <josueantoniogardasz@hotmail.com>
2023-06-03 17:14:11 -0300

    pre-release

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -30,4 +30,24 @@ function addImage() {
   }
 }
 
+function cadastrar() {
+  var nomeInput = document.getElementById("nome-input").value
+  var precoInput = document.getElementById("preco-input").value
+  var descricaoInput = document.getElementById("descricao-input").value
+
+  // Exemplo de ação: exibindo os valores no console
+  console.log("Nome:", nomeInput)
+  console.log("Preço:", precoInput)
+  console.log("Descrição:", descricaoInput)
+
+  // Adicione o código adicional para realizar o cadastro ou qualquer outra ação desejada
+
+  // Limpar os campos após o cadastro
+  document.getElementById("nome-input").value = ""
+  document.getElementById("preco-input").value = ""
+  document.getElementById("descricao-input").value = ""
+}
+
+// Evento de clique para cadastrar
+document.getElementById("cadastrar-button").addEventListener("click", cadastrar)
 
